eb07dd2796658823abc09880b994234338631460
Example: Riley Murphy <rileycmurph@outlook.com>
2022-09-12 20:32:34 -0400

    Worked on PEDAC, Method for finding median

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NumberGuesser
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            List<int> guessingRange = new List<int>();
+
+            for (int index = 1; index <= 1024; index++)
+            {
+                guessingRange.Add(index);
+            }
+
+            Console.WriteLine($"Welcome! Please think of a number from 1 to 1024. I will guess a number and you will tell me if your number is HIGHER, LOWER, or if I am CORRECT.");
+
+        }
     }
-  }
 }
+
+
+
+
